a15363e4ff9f4b351013d1ca1e4c568685fa505b
Luiz Carlos <luizvieiracarlosrex@gmail.com>
2025-04-05 15:22:50 -0300

    Inserindo Novos Elementos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,14 @@
 import { View, Text } from "react-native";
-
+// Export default é o metodo usado para dizer ao ExporRouter que esse componente é uma Rota.
 export default function Index () {
     return (
         <View>
             <Text>Siiiiiu!!</Text>
+            <Text>Cristiano Ronaldo dos Santos Alveiro</Text>
+            <Text>Nascimento: 05/02/1985</Text>
+            <Text>Endereço: Portugal,Ilha da Madeira</Text>
+            <Text>Status: Casado</Text>
+            <Text>Filhos: 5</Text>
         </View>    
-    );
+    );// Componente Text sendo usando para Renderizar um texto na tela
 }
