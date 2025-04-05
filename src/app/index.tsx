78b01fa192c57b4dd0017ebe733c4f77b09ee9d2
Luiz Carlos <luizvieiracarlosrex@gmail.com>
2025-04-05 17:23:22 -0300

    Inportando Novos Dados

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,68 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 // Export default é o metodo usado para dizer ao ExporRouter que esse componente é uma Rota.
 export default function Index () {
     return (
         <View>
-            <Text>Siiiiiu!!</Text>
-            <Text>Cristiano Ronaldo dos Santos Alveiro</Text>
-            <Text>Nascimento: 05/02/1985</Text>
-            <Text>Endereço: Portugal,Ilha da Madeira</Text>
-            <Text>Status: Casado</Text>
-            <Text>Filhos: 5</Text>
-            <Text>Time: Al nass</Text>
-            <Text>Idade: 40 Anos</Text>
-            <Text>5 Ballon d'Or</Text>
+            <Text style={style.title} >Siiiiiu!!</Text>
+            <Text style={style.title2} >Cristiano Ronaldo dos Santos Alveiro</Text>
+            <Text style={style.title3} >Nascimento: 05/02/1985</Text>
+            <Text style={style.title5}>Endereço: Portugal,Ilha da Madeira</Text>
+            <Text style={style.title6}>Status: Casado</Text>
+            <Text style={style.title7}>Filhos: 5</Text>
+            <Text style={style.title8}>Time: Al nass</Text>
+            <Text style={style.title9}>Idade: 40 Anos</Text>
+            <Text style={style.title10}>5 Ballon d'Or</Text>            
         </View>    
     );// Componente Text sendo usando para Renderizar um texto na tela
 }
+const style = StyleSheet.create({
+    title: {
+        color: "red",
+        fontSize: 30,
+    },
+
+    title2: {
+        color: "green",
+        fontSize: 24,
+    },
+
+    title3: {
+        color: "Yellow",
+        fontSize: 19,
+    },
+
+    title4: {
+        color: "blue",
+        fontSize: 19,
+    },
+
+    title5: {
+        color: "orange",
+        fontSize: 19,
+    },
+
+    title6: {
+        color: "purple",
+        fontSize: 19,
+    },
+
+    title7: {
+        color: "pink",
+        fontSize: 19,
+    },
+
+    title8: {
+        color: "brown",
+        fontSize: 19,
+    },
+
+    title9: {
+        color: "orange",
+        fontSize: 19,
+    },
+
+    title10: {
+        color: "blue",
+        fontSize: 19,
+    },
+})
